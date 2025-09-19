af8e307a4d42616d87e06bacf57a8f07c7799d20
JotaVeMr <joaovitormartinsbtos@gmail.com>
2025-09-19 15:32:07 -0300

    Praticando python

---
+++ listas/lista_resto.py
@@ -0,0 +1,13 @@
+infos = [
+    "Joao",
+    "Vitor",
+    "casado",
+    "estudante",
+    "jogador",
+    "20 anos"  
+
+]
+
+nome, _,_,_,profissao, *_ = infos # SÓ PODE USA
+
+print(f'Nome: {nome} \nProfissao: {profissao}')
